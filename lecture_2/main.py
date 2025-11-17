4d7b0a75f01bc760e0ebe3b093fdb71bf6a5e9d3
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-11-17 13:21:39 +0300

    Add get_user_info and collect_hobbies function

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,3 +1,4 @@
+CURRENT_YEAR = 2025
 
 def generate_profile(age):
     if age >= 0 and age <= 12:
@@ -6,3 +7,32 @@ def generate_profile(age):
         return "Teenager"
     else:
         return "Adult"
+
+
+def get_user_info():
+    user_name = input("Enter your full name: ")
+    birth_year_str = input("Enter your birth year: ")
+    birth_year = int(birth_year_str)
+    current_age = CURRENT_YEAR - birth_year
+
+    return user_name, birth_year, current_age
+
+
+def collect_hobbies():
+    hobbies = []
+
+    while True:
+        hobby = input("Enter a favorite hobby or type 'stop' to finish: ")
+        if hobby.lower() == "stop":
+            break
+        hobbies.append(hobby)
+
+    return hobbies
+
+def main():
+    user_name, birth_year, current_age = get_user_info()
+    hobbies = collect_hobbies()
+    life_stage = generate_profile(current_age)
+
+if __name__ == "__main__":
+    main()
